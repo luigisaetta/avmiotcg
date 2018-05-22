0779fa020c85b5e9ebcf774acb60dcb03ea9c362
Luigi Saetta <lsaetta@MBP-di-Luigi.local>
2018-05-22 10:45:41 +0200

    first commit with pom complete

--- iotgateway/src/main/java/oracle/avmcg/Main.java
+++ iotgateway/src/main/java/oracle/avmcg/Main.java
@@ -11,35 +11,44 @@ import java.net.URI;
  * Main class.
  *
  */
-public class Main {
-    // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+public class Main
+{
+	// Base URI the Grizzly HTTP server will listen on
+	public static final String BASE_URI = "http://localhost:8080/avm/";
 
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
-     * @return Grizzly HTTP server.
-     */
-    public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in oracle.avmcg package
-        final ResourceConfig rc = new ResourceConfig().packages("oracle.avmcg");
+	/**
+	 * Starts Grizzly HTTP server exposing JAX-RS resources defined in this
+	 * application.
+	 * 
+	 * @return Grizzly HTTP server.
+	 */
+	public static HttpServer startServer()
+	{
+		// create a resource config that scans for JAX-RS resources and providers
+		// in oracle.avmcg package
+		final ResourceConfig rc = new ResourceConfig().packages("oracle.avmcg");
 
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-    }
+		// create and start a new instance of grizzly http server
+		// exposing the Jersey application at BASE_URI
+		return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+	}
 
-    /**
-     * Main method.
-     * @param args
-     * @throws IOException
-     */
-    public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
-    }
+	/**
+	 * Main method.
+	 * 
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException
+	{
+		final HttpServer server = startServer();
+		
+		System.out.println(String.format(
+				"Jersey app started with WADL available at " + "%sapplication.wadl\nHit enter to stop it...",
+				BASE_URI));
+		
+		System.in.read();
+		
+		server.shutdown();
+	}
 }
-
